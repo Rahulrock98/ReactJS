090d7f630ed6720fb4fae347006de049d58c382a
Rahul Kumar <72124534+Rahulrock98@users.noreply.github.com>
2024-08-02 19:39:38 +0000

    react: createelment, evaluate variable etc..

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -5,10 +5,10 @@ import './App.css'
 import Chai from './Chai'
 
 function App(   ) {
-
+const username = "hold it.. almost done"
   return (
     <>
-    <p>chai is under process.....</p>
+    <p>chai is under process.....{username}</p>
     <Chai/>
     </>
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -5,16 +5,34 @@ import './index.css'
 
 
 
- function MyApp(){
-  return(
+function MyApp() {
+  return (
     <div>
       <h1>custom app</h1>
     </div>
   )
 
- }
+}
+
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  children: 'Click me to visit google'
+}
+const anothterElement = (
+  <a href="https://google.com" target="_blank">visit google </a>
+)
+const anotherUser = " chai aur code"
+const createElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'click me to visit google',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MyApp />
-  </React.StrictMode>,
+  createElement
 )
